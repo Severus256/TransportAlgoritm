585ba40b5ce47efc3f0ba7e8127f6ff45c4d7a7d
Severus256 <severus256@gmail.com>
2016-01-24 15:44:54 +0100

    creating form

--- src/transportalgoritm/Main.java
+++ src/transportalgoritm/Main.java
@@ -5,46 +5,61 @@
  */
 package transportalgoritm;
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.WindowConstants;
+import javax.swing.border.TitledBorder;
 
 /**
  *
  * @author Aga
  */
 public class Main {
+
     public static void main(String[] args) {
-        JFrame form = new JFrame("Транспортная задача");
-        form.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        form.setSize(500, 500);
-        form.setLocationRelativeTo(null);
-       
-        JTable table;
-        table = creatingTable(form);
+       JFrame MainForm;
+       MainForm = creatingForm();
+
         
-        form.add(new JScrollPane(table));
-        form.setVisible(true);
-        
+
+
     }
-    
+
     // тут параметром добавить размеры таблицы! 
     // отдельным методом можно заполнить стартовые значения
     public static JTable creatingTable(JFrame form) {
-        Object[][] mat = {{1,2,3}, {4,5,6}};
-        Object[] column = {"A", "B", "C"};
+
+          Object[][] mat = {{1,2,3}, {4,5,6}};
+         Object[] column = {"A", "B", "C"};
         
-        JTable table = new JTable(mat, column);
-        table.setBackground(form.getBackground());
-        table.setShowGrid(false);
-        table.setSize(300, 300);
-        return table;
+         JTable table = new JTable(mat, column);
+         table.setBackground(form.getBackground());
+         table.setShowGrid(false);
+         table.setSize(300, 300);
+         return table;
+         
     }
-    
-    
-    
-    
-    
-    
+    public static JFrame creatingForm() {
+        JFrame form = new JFrame("Транспортная задача");
+        JTable table;
+        table = creatingTable(form);
+        form.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        form.setSize(500, 500);
+        form.setLocationRelativeTo(null);
+        JPanel panel = new JPanel();
+        panel.setBorder(new TitledBorder("Data:"));
+        panel.setPreferredSize(new Dimension(250, 100));
+        table.setBackground(panel.getBackground());
+        panel.add(table);
+        form.add(panel);
+        form.pack();
+        form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        form.setVisible(true);
+
+        return form;
+    }
+
 }
