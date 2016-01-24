f94b4d6869be66eb628b077bf83bc9a711ef8ed2
Severus256 <severus256@gmail.com>
2016-01-24 15:59:32 +0100

    Доделать создание таблицы с начальными данными

--- src/transportalgoritm/Main.java
+++ src/transportalgoritm/Main.java
@@ -20,45 +20,47 @@ import javax.swing.table.AbstractTableModel;
 public class Main {
 
     public static void main(String[] args) {
-       JFrame MainForm;
-       MainForm = creatingForm();
+        JFrame MainForm;
+        MainForm = creatingForm();
+        MainForm.setVisible(true);
     }
 
     // тут параметром добавить размеры таблицы! 
     // отдельным методом можно заполнить стартовые значения
     public static JTable creatingTable(JFrame form) {
-        
+
+        int Data[][] = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}}; // Testing Data !
+
         JTable table = new JTable(new AbstractTableModel() {
 
             @Override
             public int getRowCount() {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                return Data.length;
             }
 
             @Override
             public int getColumnCount() {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                return Data[0].length;
             }
 
             @Override
             public Object getValueAt(int rowIndex, int columnIndex) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                return Data[rowIndex][columnIndex];
             }
-            
+
         });
-        
-       /*   Object[][] mat = {{1,2,3}, {4,5,6}};
+
+        /*   Object[][] mat = {{1,2,3}, {4,5,6}};
          Object[] column = {"A", "B", "C"};
         
          JTable table = new JTable(mat, column);
          table.setBackground(form.getBackground());
          table.setShowGrid(false);
          table.setSize(300, 300);*/
-        
-         
-         return table;
-         
+        return table;
+
     }
+
     public static JFrame creatingForm() {
         JFrame form = new JFrame("Транспортная задача");
         JTable table;
@@ -74,7 +76,7 @@ public class Main {
         form.add(panel);
         form.pack();
         form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        form.setVisible(true);
+        
 
         return form;
     }
